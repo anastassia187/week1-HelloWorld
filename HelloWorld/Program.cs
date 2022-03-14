293ab430522305740925484c5f0ee3aff215a4f7
Anastassia Kuhartsik <nastjonka187@gmail.com>
2022-03-14 15:29:47 +0200

    add my first c# project

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string userFirstname = Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            string userLastname = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + userFirstname + " " + userLastname + "!");
+
+
+
+
+
+
+
+
+
+           
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+    }
+}
---
+++ StringInterpolation/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string userFirstname = Console.ReadLine();
+            Console.WriteLine("what is your last name?");
+            string userLastname = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {userFirstname} {userLastname}!");//string iterpolation
+
+            //Console.WriteLine("Hello, " + userFirstname + " " + userLastname + "!");//string concantenation
+
+        }
+    }
+}
